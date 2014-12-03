3b9745e27c4870e25ac884676614ee4cdc018370
tblum <tblum@counsyl.com>
2014-12-02 19:21:08 -0500

    Initial Commit

---
+++ Chess Tactics/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Chess Tactics
+//
+//  Created by tblum on 12/2/14.
+//  Copyright (c) 2014 tblum. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
